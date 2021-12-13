4c371191dd3463a48470c7e057a50234d3baa329
pepeillo <jaferriz@gmail.com>
2021-12-13 22:01:45 +0100

    make-up

--- app/src/main/java/es/jaf/myshortcuts/NewShortcutActivity.java
+++ app/src/main/java/es/jaf/myshortcuts/NewShortcutActivity.java
@@ -2,16 +2,8 @@ package es.jaf.myshortcuts;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -64,30 +56,7 @@ public class NewShortcutActivity extends Activity {
         });
 
         findViewById(R.id.btnFile).setOnClickListener(v -> {
-            String current = txtPath.getText().toString();
             showFileChooser();
-            /*
-            DialogProperties properties = new DialogProperties();
-            properties.selection_mode = DialogConfigs.SINGLE_MODE;
-            properties.selection_type = DialogConfigs.FILE_SELECT;
-            properties.root = Environment.getExternalStorageDirectory();
-            properties.error_dir = properties.root;
-            if (current.length() == 0 || current.toLowerCase().startsWith("http://") || current.toLowerCase().startsWith("https://")) {
-                properties.offset = properties.root; //new File(DialogConfigs.DEFAULT_DIR);
-            } else {
-                properties.offset = new File(current).getParentFile();
-            }
-            properties.extensions = null;
-
-            FilePickerDialog dialog = new FilePickerDialog(NewShortcutActivity.this ,properties);
-            dialog.setTitle(R.string.select_file);
-            dialog.show();
-
-            dialog.setDialogSelectionListener(files -> {
-                File file = new File(files[0]);
-                txtPath.setText(file.getAbsolutePath());
-            });
-             */
         });
     }
 
@@ -138,108 +107,8 @@ public class NewShortcutActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // If the user doesn't pick a file just return
-        if (requestCode != 111 || resultCode != RESULT_OK) {
-            return;
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            txtPath.setText(FileUtils.getPath(this, data.getData()));
         }
-        txtPath.setText(getPath(this, data.getData()));
     }
-
-    public String getPath(Context context, Uri uri) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (DocumentsContract.isDocumentUri(context, uri)) { // DocumentProvider
-                //System.out.println("hhhhhhhhhh isDocumentUri" );
-                if (isExternalStorageDocument(uri)) { // ExternalStorageProvider
-                    //System.out.println("hhhhhhhhhh isExternalStorageDocument" );
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-
-                    if ("primary".equalsIgnoreCase(type)) {
-                        return Environment.getExternalStorageDirectory() + "/" + split[1];
-                    }
-
-                } else if (isDownloadsDocument(uri)) { // DownloadsProvider
-                    //System.out.println("hhhhhhhhhh isDownloadsDocument" );
-                    final String id = DocumentsContract.getDocumentId(uri);
-                    final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                    return getDataColumn(context, contentUri, null, null);
-
-                } else if (isMediaDocument(uri)) { // MediaProvider
-                    //System.out.println("hhhhhhhhhh isMediaDocument" );
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-                    Uri contentUri = null;
-                    if ("image".equals(type)) {
-                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("video".equals(type)) {
-                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("audio".equals(type)) {
-                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                    }
-                    final String selection = "_id=?";
-                    final String[] selectionArgs = new String[]{split[1]};
-                    return getDataColumn(context, contentUri, selection, selectionArgs);
-                }
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) { // MediaStore (and general)
-            //System.out.println("hhhhhhhhhh isContent" );
-            if (isGooglePhotosUri(uri)) {
-                //System.out.println("hhhhhhhhhh isGooglePhotosUri" );
-                return uri.getLastPathSegment();
-            }
-            return getDataColumn(context, uri, null, null);
-
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) { // File
-            //System.out.println("hhhhhhhhhh isFile" );
-            return uri.getPath();
-        }
-        return null;
-    }
-
-    public String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
-            }
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-        return null;
-    }
-
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is Google Photos.
-     */
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
-    }
-}
+ }
